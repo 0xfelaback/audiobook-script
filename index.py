a4bf204fbc84f26128f05aaa3eb6f92272d398e0
Daniel Onuchukwu <114066984+0xfelaback@users.noreply.github.com>
2025-09-30 11:06:42 +0100

    fix: save full audiobook instead of only last page --try again

--- index.py
+++ index.py
@@ -3,17 +3,22 @@ import PyPDF3
 
 with open('book.pdf', 'rb') as book:
 
-    full_text = ""
+    reader = PyPDF3.PdfFileReader(book)
+    for i,page in enumerate(reader.pages):
+        text = page.extract_text()
+    with open(f"chapter_{i+1}.txt", "w", encoding="utf-8") as f:
+        f.write(text)
 
     reader = PyPDF3.PdfFileReader(book)
     
-    audio_reader = pyttsx3.init()
-    audio_reader.setProperty("rate", 100)
     
+    audio_reader = pyttsx3.init()
+    full_text = ""
+    audio_reader.setProperty("rate", 100)
     for page in range(reader.numPages):
         next_page = reader.getPage(page)
         content = next_page.extractText()
         full_text += content
-        
-    audio_reader.save_to_file(content, "myaudiobook.mp3")
+
+    audio_reader.save_to_file(full_text, "myaudiobook.mp3")
     audio_reader.runAndWait()
